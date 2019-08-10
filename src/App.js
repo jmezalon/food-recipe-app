6ed49444a75f804ae9582578eb4fcc7579eb8e4e
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-08-10 07:25:58 -0400

    trying to push

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from "react";
+import secret from "./secret.js";
+import Recipe from "./Recipe";
+import "./App.css";
+
+const App = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
+  useEffect(() => {
+    getRecipes(query);
+  }, [query]);
+
+  const getRecipes = async query => {
+    const response = await fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.edamam.com/search?q=${query}&app_id=${secret.my_id}&app_key=${secret.apikey}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+  return (
+    <div className="App">
+      <form className="search-form" onSubmit={getSearch}>
+        <input
+          className="search-bar"
+          type="text"
+          value={search}
+          onChange={updateSearch}
+        />
+        <button className="search-button">search</button>
+      </form>
+      <div className="recipes">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.calories}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
